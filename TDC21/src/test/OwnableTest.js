3744f35b2d0ebddede3be55650912abe5bd10168
PRDeving <pablo.deving@gmail.com>
2021-09-14 19:29:21 +0200

    Tdc21 #16 Ownable contract (#18)
    * initial contract and test
    
    * TDC21-16 tests ownership and ownershipTransfer
    
    * TDC21-16 tests ownershipTransferred emits event
    
    * TDC21-16 not owner cannot transfer ownership
    
    * finish Ownable contract
    
    * package.json
    
    * add negate ownership transfer to nil
    
    Co-authored-by: casanchez <carlos.sanchez@lastminute.com>

---
+++ TDC21/src/test/OwnableTest.js
@@ -0,0 +1,86 @@
+const truffleAssert = require('truffle-assertions');
+require('chai').use(require('chai-as-promised')).should();
+
+const Ownable = artifacts.require("Ownable");
+
+const ethBalance = async (add) => web3.utils.fromWei(await web3.eth.getBalance(add), 'ether');
+
+const event = {
+    emitted: (result, name, obj, msg = '') => truffleAssert.eventEmitted(result, name, eventFilter(obj), msg),
+    notEmitted: (result, name, obj, msg = '') => truffleAssert.eventNotEmitted(result, name, eventFilter(obj), msg),
+}
+const eventFilter = obj => !obj ? null : (ev) => {
+    const k = Object.keys(obj)
+    for (let i = 0; i < k.length; i++) {
+        if (ev[k[i]] != obj[k[i]]) return false
+    }
+    return true
+}
+
+contract('Ownable', (accounts) => {
+    describe('deployment', () => {
+        let instance;
+
+        before(async () => {
+            instance = await Ownable.deployed();
+        })
+
+        it('deploys successfully', async () => {
+            assert.notEqual(instance.address, '')
+            assert.notEqual(instance.address, 0x0)
+            assert.notEqual(instance.address, undefined)
+            assert.notEqual(instance.address, null)
+            assert.equal(await ethBalance(instance.address), 0)
+        })
+
+    })
+
+    describe('contract', () => {
+        let instance;
+        beforeEach(async () => {
+            instance = await Ownable.deployed();
+        })
+
+        it('is owned by first address', async () => {
+            assert.equal(await instance.owner(), accounts[0])
+        })
+
+        it('is transferred properly', async () => {
+            assert.equal(await instance.owner(), accounts[0])
+
+            let resultAddress = await instance.transferOwnership(accounts[1])
+            assert.equal(await instance.owner(), accounts[1])
+
+            event.emitted(resultAddress, 'OwnershipTransferred', {
+                previousOwner: accounts[0],
+                newOwner: accounts[1]
+            }, 'Contract should return the correct event.');
+        })
+
+        it('cannot transfer ownership if sender is not the owner', async () => {
+            assert.equal(await instance.owner(), accounts[1])
+
+            const err = 'VM Exception while processing transaction: revert'
+
+           await instance.transferOwnership(accounts[2], {
+                from: accounts[4]
+            }).should.be.rejectedWith(err);
+
+            assert.equal(await instance.owner(), accounts[1])
+        })
+
+        it('cannot transfer ownership if new owner is 0x0', async () => {
+            assert.equal(await instance.owner(), accounts[1])
+
+            const err = 'VM Exception while processing transaction: revert'
+
+           await instance.transferOwnership('0x0000000000000000000000000000000000000000', {
+                from: accounts[1]
+            }).should.be.rejectedWith(err);
+
+            assert.equal(await instance.owner(), accounts[1])
+        })
+    })
+
+})
+
