bf4ebfa8cc65431fac6781ae1b0cf4f378cbb59a
Yahir <yahir.penarrubia@gmail.com>
2021-10-04 18:21:22 +0200

    TDC21-17 refactor

--- TDC21/src/test/Ownable.test.js
+++ TDC21/src/test/Ownable.test.js
@@ -4,12 +4,11 @@ const Ownable = artifacts.require("Ownable");
 
 const { Emitted } = require('./helpers/events')
 const { ethBalance } = require('./helpers/balance')
-const { deadAddress } = require('./utils/constants')
+const { DEAD_ADDRESS, ERROR_MESSAGE } = require('./utils/constants')
 
 contract('Ownable', (accounts) => {
 
     let instance;
-
     beforeEach(async () => {
         instance = await Ownable.new();
     })
@@ -24,37 +23,37 @@ contract('Ownable', (accounts) => {
         })
     })
 
-    describe('contract', () => {
-
+    describe('owner', () => {
         it('is owned by first address', async () => {
-            assert.equal(await instance.owner(), accounts[0])
+            const result = await instance.owner()
+            assert.typeOf(result, 'string')
+            assert.equal(result, accounts[0])
         })
+    })
 
+    describe('transferOwnership', () => {
         it('is transferred properly', async () => {
-            let resultAddress = await instance.transferOwnership(accounts[1])
+            const result = await instance.transferOwnership(accounts[1])
             assert.equal(await instance.owner(), accounts[1])
 
-            Emitted(resultAddress, 'OwnershipTransferred', {
+            Emitted(result, 'OwnershipTransferred', {
                 previousOwner: accounts[0],
                 newOwner: accounts[1]
             }, 'Contract should return the correct event.');
         })
 
         it('cannot transfer ownership if sender is not the owner', async () => {
-            const err = 'VM Exception while processing transaction: revert'
-
             await instance.transferOwnership(accounts[2], {
                 from: accounts[4]
-            }).should.be.rejectedWith(err);
+            }).should.be.rejectedWith(ERROR_MESSAGE);
 
             assert.equal(await instance.owner(), accounts[0])
         })
 
-        it('cannot transfer ownership if new owner is 0x0', async () => {const err = 'VM Exception while processing transaction: revert'
-            await instance.transferOwnership(deadAddress, { from: accounts[1] }).should.be.rejectedWith(err);
+        it('cannot transfer ownership if new owner is 0x0', async () => {
+            await instance.transferOwnership(DEAD_ADDRESS, { from: accounts[1] }).should.be.rejectedWith(ERROR_MESSAGE);
             assert.equal(await instance.owner(), accounts[0])
         })
     })
-
 })
 
--- TDC21/src/test/utils/constants.js
+++ TDC21/src/test/utils/constants.js
@@ -1,5 +1,9 @@
-const deadAddress = "0x0000000000000000000000000000000000000000";
+const DEAD_ADDRESS = "0x0000000000000000000000000000000000000000";
+const ERROR_MESSAGE = 'VM Exception while processing transaction: revert';
+const CREATE_CONTRACT_COST = 100;
 
 module.exports = {
-    deadAddress
+    DEAD_ADDRESS,
+    ERROR_MESSAGE,
+    CREATE_CONTRACT_COST
 };
